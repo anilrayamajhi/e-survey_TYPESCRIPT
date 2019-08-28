0ca81abd92220552a6337658efae7f731affa514
~A <arayamajhi@endertech.com>
2019-08-28 13:18:30 -0700

    initial setup and dir structure

---
+++ client/src/containers/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        {/* <img src={logo} className="App-logo" alt="logo" />
+        <h2>Supp Ninja</h2> */}
+      </header>
+    </div>
+  );
+}
+
+export default App;
